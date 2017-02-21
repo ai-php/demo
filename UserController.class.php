47bcaac99efbfa6a38ff94b3512fb4a3d2801044
demo <demo@example.com>
2017-02-21 09:19:27 +0800

    添加描述

---
+++ UserController.class.php
@@ -0,0 +1,33 @@
+<?php 
+
+	/**
+	* 
+	*/
+	class ClassName extends AnotherClass
+	{
+		
+		function __construct(argument)
+		{
+			# code...
+		}
+
+		public function addUser($value='')
+		{
+			# code...
+		}
+
+		public function delUser($value='')
+		{
+			# code...
+		}
+
+		public function editUser($value='')
+		{
+			# code...
+		}
+	}
+
+	
+
+
+ ?>
